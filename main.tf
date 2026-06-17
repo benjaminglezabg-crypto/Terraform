0c5350a3f25385f55d669eec83b78834082c4510
benjaminglezabg-crypto <benjamingleza.bg@gmail.com>
2026-06-16 21:46:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Buscar la AMI más reciente de Amazon Linux 2023
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# Security Group para la EC2
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-test-sg"
+  description = "Allow SSH access"
+  vpc_id      = "vpc-0b501d57c8b31ebe3"
+
+  tags = {
+    Name = "ec2-test-sg"
+  }
+}
+
+# Regla SSH desde tu IP publica
+resource "aws_vpc_security_group_ingress_rule" "ssh_from_my_ip" {
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = "177.249.175.66/32"
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
+  description       = "Allow SSH from my IP"
+}
+
+# Salida a internet
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+# EC2 sencilla
+resource "aws_instance" "test_ec2" {
+  ami                         = data.aws_ami.amazon_linux_2023.id
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-063d07d493700917c"
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "new"
+
+  tags = {
+    Name = "ec2-test-terraform"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.test_ec2.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.test_ec2.private_ip
+}
+
+output "security_group_id" {
+  value = aws_security_group.ec2_sg.id
+}
