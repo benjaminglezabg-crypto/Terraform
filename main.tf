30769fc763afc6c9b8942ccc6293e0ace92d62ec
benjaminglezabg-crypto <benjamingleza.bg@gmail.com>
2026-06-16 22:21:01 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,13 +11,6 @@ terraform {
       version = "~> 6.0"
     }
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
 
 provider "aws" {
   region = "us-east-1"
